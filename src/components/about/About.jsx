a928203b7afd40e44e2f3ff671f21d1d2763ba74
Salvador <rodavlasxp@gmail.com>
2022-09-15 14:06:55 -0400

    images changed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assets/me2.jpg'
+import ME from '../../assets/yo.jpg'
 import {FaAward} from 'react-icons/fa'
 import {FiUsers} from 'react-icons/fi'
 import {VscFolderLibrary} from 'react-icons/vsc'
